a576e0f17f0ef3cf2195e87bf5394785acf37b2d
jiriklepl <jiriklepl@seznam.cz>
2020-10-21 00:38:26 +0200

    minor fix

--- include/Duration.hpp
+++ include/Duration.hpp
@@ -18,15 +18,15 @@ class Duration {
     friend std::ostream& operator<<(std::ostream&, const Duration&);
 
  private:
-    static constexpr std::size_t byte_length = 7;
-    static constexpr std::size_t byte_mask = (1ULL << byte_length) - 1;
+    static constexpr std::size_t byte_size = 7;
+    static constexpr std::size_t byte_mask = (1ULL << byte_size) - 1;
     std::size_t _length;
 
     template<std::size_t where>
     std::ostream&
     print_bytes(std::ostream& stream, unsigned char byte_mess = 0) const noexcept
     {
-        const unsigned char byte = (_length >> (where * byte_length)) & byte_mask;
+        const unsigned char byte = (_length >> (where * byte_size)) & byte_mask;
         byte_mess |= byte;
 
         if (byte_mess) stream.put(byte | (byte_mask + 1));
@@ -45,7 +45,7 @@ Duration::print_bytes<0>(std::ostream& stream, unsigned char) const noexcept
 inline std::ostream&
 operator<<(std::ostream& stream, const Duration& duration)
 {
-    return duration.print_bytes<sizeof(std::size_t) * 8 / 7>(stream);
+    return duration.print_bytes<sizeof(std::size_t) * 8 / Duration::byte_size>(stream);
 }
 
 #endif  // DURATION_HPP_
