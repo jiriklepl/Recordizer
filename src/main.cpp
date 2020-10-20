f84f020a79f5740ca33906b7196d7df7c46c4685
jiriklepl <jiriklepl@seznam.cz>
2020-10-21 00:38:48 +0200

    main for testing Duration

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
-#include <iostream>
+#include <fstream>
+
+#include "Duration.hpp"
+
 
 int main(int argc, char *argv[])
 {
-	std::cout << "Hello world!" << std::endl;
+	std::ofstream soubor("soubor.txt");
+	soubor << Duration(0x0);
+	soubor << Duration(0x40);
+	soubor << Duration(0x7F);
+	soubor << Duration(0x80);
+	soubor << Duration(0x2000);
+	soubor << Duration(0x3FFF);
+	soubor << Duration(0x4000);
+	soubor << Duration(0x100000);
+	soubor << Duration(0x1FFFFF);
+	soubor << Duration(0x200000);
+	soubor << Duration(0x8000000);
+	soubor << Duration(0xFFFFFFF);
+	std::cout << "All sheet written to!" << std::endl;
 }
