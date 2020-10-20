3c910b09034d0fb41ed20420c344c848789110cd
jiriklepl <jiriklepl@seznam.cz>
2020-10-21 01:01:26 +0200

    clang format

--- include/Duration.hpp
+++ include/Duration.hpp
@@ -1,51 +1,50 @@
 #ifndef DURATION_HPP_
 #define DURATION_HPP_
+
 #include <iostream>
 
 class Duration {
- public:
-    explicit Duration(std::size_t length) noexcept
-    : _length{length} {
-    }
+public:
+  explicit Duration(std::size_t length) noexcept : _length{length} {}
 
-    Duration() noexcept
-    : _length{0} {
-    }
+  Duration() noexcept : _length{0} {}
 
-    std::size_t length() const noexcept { return _length; }
-    void set_length(std::size_t length) noexcept { _length = length; }
+  std::size_t length() const noexcept { return _length; }
+  void set_length(std::size_t length) noexcept { _length = length; }
 
-    friend std::ostream& operator<<(std::ostream&, const Duration&);
+  friend std::ostream &operator<<(std::ostream &, const Duration &);
 
- private:
-    static constexpr std::size_t byte_size = 7;
-    static constexpr std::size_t byte_mask = (1ULL << byte_size) - 1;
-    std::size_t _length;
+private:
+  static constexpr std::size_t byte_size = 7;
+  static constexpr std::size_t byte_mask = (1ULL << byte_size) - 1;
+  std::size_t _length;
 
-    template<std::size_t where>
-    std::ostream&
-    print_bytes(std::ostream& stream, unsigned char byte_mess = 0) const noexcept
-    {
-        const unsigned char byte = (_length >> (where * byte_size)) & byte_mask;
-        byte_mess |= byte;
+  template <std::size_t where>
+  std::ostream &print_bytes(std::ostream &stream,
+                            unsigned char byte_mess = 0) const noexcept {
+    const unsigned char byte = (_length >> (where * byte_size)) & byte_mask;
+    byte_mess |= byte;
 
-        if (byte_mess) stream.put(byte | (byte_mask + 1));
-        return print_bytes<where - 1>(stream, byte_mess);
-    }
+    if (byte_mess)
+      stream.put(byte | (byte_mask + 1));
+
+    return print_bytes<where - 1>(stream, byte_mess);
+  }
 };
 
-template<>
-inline std::ostream&
-Duration::print_bytes<0>(std::ostream& stream, unsigned char) const noexcept
-{
-    stream.put(_length & byte_mask);
-    return stream;
+template <>
+inline std::ostream &Duration::print_bytes<0>(std::ostream &stream,
+                                              unsigned char) const noexcept {
+  stream.put(_length & byte_mask);
+  return stream;
 }
 
-inline std::ostream&
-operator<<(std::ostream& stream, const Duration& duration)
-{
-    return duration.print_bytes<sizeof(std::size_t) * 8 / Duration::byte_size>(stream);
+inline std::ostream &operator<<(std::ostream &stream,
+                                const Duration &duration) {
+  constexpr std::size_t bytes =
+      (sizeof(std::size_t) * 8 + (Duration::byte_size - 1)) /
+      Duration::byte_size;
+  return duration.print_bytes<bytes - 1>(stream);
 }
 
-#endif  // DURATION_HPP_
+#endif // DURATION_HPP_
--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,19 @@
 
 #include "Duration.hpp"
 
-
-int main(int argc, char *argv[])
-{
-	std::ofstream soubor("soubor.txt");
-	soubor << Duration(0x0);
-	soubor << Duration(0x40);
-	soubor << Duration(0x7F);
-	soubor << Duration(0x80);
-	soubor << Duration(0x2000);
-	soubor << Duration(0x3FFF);
-	soubor << Duration(0x4000);
-	soubor << Duration(0x100000);
-	soubor << Duration(0x1FFFFF);
-	soubor << Duration(0x200000);
-	soubor << Duration(0x8000000);
-	soubor << Duration(0xFFFFFFF);
-	std::cout << "All sheet written to!" << std::endl;
+int main(int argc, char *argv[]) {
+  std::ofstream soubor("soubor.txt");
+  soubor << Duration(0x0);
+  soubor << Duration(0x40);
+  soubor << Duration(0x7F);
+  soubor << Duration(0x80);
+  soubor << Duration(0x2000);
+  soubor << Duration(0x3FFF);
+  soubor << Duration(0x4000);
+  soubor << Duration(0x100000);
+  soubor << Duration(0x1FFFFF);
+  soubor << Duration(0x200000);
+  soubor << Duration(0x8000000);
+  soubor << Duration(0xFFFFFFF);
+  std::cout << "All sheet written to!" << std::endl;
 }
