6c75ec726917e4c0e6f8bea9ffb31e6624a3109c
jiriklepl <jiriklepl@seznam.cz>
2020-10-27 23:55:13 +0100

    test file simplified

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,28 @@
 #include <fstream>
 #include <iostream>
 
-#include "Transformator.hpp"
+#include "TransformatorBuilder.hpp"
 #include "NoteConstants.hpp"
 
 int main(int argc, char *argv[]) {
   std::ofstream soubor{"soubor.mid", std::ios::binary};
   Transformator trans{};
+  TransformatorBuilder builder{trans, 0};
 
-  trans.append_event<ProgramChange>(0, 0, 75);
-  trans.append_note(100, 0, {100, C + 4, 100});
-  trans.append_note(0, 0, {100, E + 4, 100});
-  trans.append_note(0, 0, {100, C + 4, 100});
-  trans.append_note(0, 0, {100, E + 4, 100});
-  trans.append_note(0, 0, {200, G + 4, 100});
-  trans.append_note(0, 0, {100, G + 4, 100});
-  trans.append_note(0, 0, {100, G + 4, 100});
+  builder << ProgramChange{75}
+          << shift(100)
+          << Note{200, A + 4, 100}
+          << Note{50, H + 4, 100}
+          << Note{50, A + 4, 100}
 
-  trans.append_note(0, 0, {100, C + 4, 100});
-  trans.append_note(0, 0, {100, E + 4, 100});
-  trans.append_note(0, 0, {100, C + 4, 100});
-  trans.append_note(0, 0, {100, E + 4, 100});
-  trans.append_note(0, 0, {200, D + 4, 100});
-  trans.append_note(0, 0, {100, D + 4, 100});
-  trans.append_note(0, 0, {100, D + 4, 100});
+          << Note{100, E + 4, 100}
+          << Note{200, G + 4, 100}
+          << Note{50, F + 4, 100}
+          << Note{50, E + 4, 100}
+
+          << Note{300, D + 4, 100}
+          << Note{50, D + 4, 100}
+          << Note{50, D + 4, 100};
 
   soubor << "MThd";
   soubor.put(0);
@@ -35,7 +34,7 @@ int main(int argc, char *argv[]) {
   soubor.put(0);
   soubor.put(1);
   soubor.put(0);
-  soubor.put(100);
+  soubor.put(50);
   soubor << trans;
   std::cout << "File written!" << std::endl;
 }
